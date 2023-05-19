c6b580c7f2328a845296a04485ec3efbceb299d4
saleh1992 <salehsites@gmail.com>
2023-05-19 22:47:33 +0300

    make code more clean and readable

--- src/App.js
+++ src/App.js
@@ -10,62 +10,69 @@ function App() {
   const APP_ID = '4a4959e6';
   const APP_KEY = '1a6290d1bc108e4cbbffa06308c7ffd4';
   const [recipe, setRecipe] = useState([]);
-  const [search, setSearch] = useState('')
-  const [from, setFrom] = useState(0)
-  const [to, setTo] = useState(12)
+  const [search, setSearch] = useState('');
+  const [from, setFrom] = useState(0);
+  const [to, setTo] = useState(12);
 
   useEffect(() => {
     getRecipes();
-  }, [])
+  }, []);
+
   const getRecipes = async (FROM, TO) => {
-    console.log('FROM', FROM, 'TO', !TO);
     if (!TO) {
-      // reset load more or pagination in new search
+      // Reset load more or pagination in new search
       TO = 12;
-      setTo(12)
+      setTo(12);
     }
     const response = await axios.get(`https://api.edamam.com/search?q=${search || 'meet'}&app_id=${APP_ID}&app_key=${APP_KEY}&from=${FROM || from}&to=${TO || to}`);
-    console.log(response.data.hits);
+    const responseData = response.data.hits;
 
     if (FROM && TO) {
-      // Load more recipe
-      setRecipe([...recipe, ...response.data.hits])
+      // Load more recipes
+      setRecipe([...recipe, ...responseData]);
     } else {
-      // search for new recipe
-      setRecipe(response.data.hits)
+      // Search for new recipes
+      setRecipe(responseData);
     }
+  };
 
-  }
-  const handelSearch = (e) => {
-    setSearch(e.target.value)
-  }
-  const handelSubmit = (e) => {
+  const handleSearch = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
     e.preventDefault();
     getRecipes();
-  }
-  const handelLoadMore = () => {
+  };
+
+  const handleLoadMore = () => {
     const newFrom = to;
     const newTo = to + 12;
     setTo(newTo);
     getRecipes(newFrom, newTo);
-  }
+  };
+
   return (
     <div className="App">
-
-      <header>
+      <header className="container m-auto">
         <Navbar />
       </header>
 
-
-      <form class="input-group justify-center mt-28 pb-7" onSubmit={handelSubmit}>
-        <input type="text" style={{ width: '90%' }} placeholder="Search…" value={search} onInput={handelSearch} class="input input-bordered bg-primary-content border-primary-content" />
-        <button class="btn btn-ghost bg-primary-content border-primary-content" type='submit'>
+      <form className="input-group justify-center pt-28" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          style={{ width: '80%' }}
+          placeholder="Search…"
+          value={search}
+          onInput={handleSearch}
+          className="input input-bordered bg-primary-content border-primary-content"
+        />
+        <button className="btn btn-ghost bg-primary-content border-primary-content" type="submit">
           <FiSearch />
         </button>
       </form>
 
-
-      <div className="flex gap-11 justify-center flex-wrap m-auto">
+      <div className="container gap-11 flex justify-center flex-wrap m-auto">
         {recipe.map((recipe) => (
           <RecipeCard
             key={recipe.recipe.label}
@@ -77,9 +84,9 @@ function App() {
         ))}
       </div>
 
-      <button class="btn btn-ghost my-6" onClick={handelLoadMore}>Load More</button>
-
-
+      <button className="btn btn-ghost my-6" onClick={handleLoadMore}>
+        Load More
+      </button>
     </div>
   );
 }
