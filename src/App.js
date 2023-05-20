5200ef04be6804fd3253890c56f64408210051cc
saleh1992 <salehsites@gmail.com>
2023-05-20 16:24:27 +0300

    add pageNumbers and try and catch to handel erro andd optimaze style

--- src/App.js
+++ src/App.js
@@ -4,35 +4,51 @@ import axios from 'axios';
 import Navbar from './components/Navbar';
 import RecipeCard from './components/RecipeCard';
 import { FiSearch } from 'react-icons/fi';
-import Footer from './components/Footer';
+
 
 function App() {
   const APP_ID = '4a4959e6';
   const APP_KEY = '1a6290d1bc108e4cbbffa06308c7ffd4';
+  const pageNumber = 12;
   const [recipe, setRecipe] = useState([]);
   const [search, setSearch] = useState('');
   const [from, setFrom] = useState(0);
-  const [to, setTo] = useState(12);
+  const [to, setTo] = useState(pageNumber);
+  const [response, setResponse] = useState({});
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
-    getRecipes();
+    return () => {
+      getRecipes();
+    }
   }, []);
 
   const getRecipes = async (FROM, TO) => {
-    if (!TO) {
-      // Reset load more or pagination in new search
-      TO = 12;
-      setTo(12);
-    }
-    const response = await axios.get(`https://api.edamam.com/search?q=${search || 'meet'}&app_id=${APP_ID}&app_key=${APP_KEY}&from=${FROM || from}&to=${TO || to}`);
-    const responseData = response.data.hits;
+    try {
+      if (!TO) {
+        // Reset load more or pagination in new search
+        TO = pageNumber;
+        setTo(pageNumber);
+      }
 
-    if (FROM && TO) {
-      // Load more recipes
-      setRecipe([...recipe, ...responseData]);
-    } else {
-      // Search for new recipes
-      setRecipe(responseData);
+      const response = await axios.get(`https://api.edamam.com/search?q=${search || 'meet'}&app_id=${APP_ID}&app_key=${APP_KEY}&from=${FROM || from}&to=${TO || to}`);
+      setResponse(response);
+      const responseData = response.data.hits;
+      setLoading(true);
+      console.log("response", response);
+
+      if (FROM && TO) {
+        // Load more recipes
+        setRecipe([...recipe, ...responseData]);
+      } else {
+        // Search for new recipes
+        setRecipe(responseData);
+      }
+    } catch (error) {
+      // Handle the error here
+      setLoading(false);
+      console.error("Error fetching recipes:", error);
+      // Optionally, you can set an error state or show a message to the user
     }
   };
 
@@ -47,7 +63,7 @@ function App() {
 
   const handleLoadMore = () => {
     const newFrom = to;
-    const newTo = to + 12;
+    const newTo = to + pageNumber;
     setTo(newTo);
     getRecipes(newFrom, newTo);
   };
@@ -58,19 +74,19 @@ function App() {
         <Navbar />
       </header>
 
-      <form className="input-group justify-center pt-28" onSubmit={handleSubmit}>
+      <form className="flex justify-center gap-3 items-center pb-6 pt-28" onSubmit={handleSubmit}>
         <input
           type="text"
-          style={{ width: '80%' }}
+          style={{ width: '85%' }}
           placeholder="Search…"
           value={search}
           onInput={handleSearch}
-          className="input input-bordered bg-primary-content border-primary-content"
+          className="input bg-primary-content "
         />
-        <button className="btn btn-ghost bg-primary-content border-primary-content" type="submit">
-          <FiSearch />
-        </button>
+        <span className='flex bg-primary-content'>({loading ? response.data.count : null} recipes)</span>
+
       </form>
+
 
       <div className="container gap-11 flex justify-center flex-wrap m-auto">
         {recipe.map((recipe) => (
